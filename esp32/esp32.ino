2ca2559f96ba2d606d428c64ef1554ae7ca028f2
abbfelarb <felicia.arborelius@abbgymnasiet.se>
2025-05-13 11:52:00 +0200

    esp32 code skeleton

---
+++ esp32/esp32.ino
@@ -0,0 +1,21 @@
+#include <Arduino.h>
+
+void setup() {
+  // set up bluetooth connection
+
+  // set up motor pins
+
+  // set up sensors
+
+}
+
+void loop() {
+  // read bluetooth values, set target
+
+  // read sensor values (current state)
+
+  // calculate response
+
+  // write to motors
+
+}
